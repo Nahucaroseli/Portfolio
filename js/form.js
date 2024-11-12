1d6df59d19deb77e2d153ee35d615eae206ee432
Nahu Caroseli <nahucaroseli2100@gmail.com>
2024-11-12 16:25:07 -0300

    fix: script link

--- js/form.js
+++ js/form.js
@@ -1,27 +1,28 @@
-emailjs.init('8Bp9YlTNSZvTdjGmw');
+emailjs.init('cw-e0ltA2aPfvFsJC');
 
-    document.getElementById('form').addEventListener('submit', function (event) {
-      event.preventDefault();
-      
-      const serviceID = 'service_nuym0gl';
-      const templateID = 'template_45d3wim';
-
-      const name = document.getElementById("name");
-      const message = document.getElementById("message");
-      const email_id = document.getElementById("email");
-      
-      emailjs.send(serviceID, templateID, {
-        from_name: name.value,
-        message: message.value,
-        email_id: email_id.value
-      })
-      .then((response) => {
-        alert("Mensaje Enviado");
-        name.innerHTML = "";
-        message.innerHTML = "";
-        email_id.innerHTML = "";
-      }, (err) => {
-        alert("Mensaje no enviado");
-      });
-    });
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault();
   
+  const serviceID = 'service_nuym0gl';
+  const templateID = 'template_45d3wim';
+
+  const name = document.getElementById("name");
+  const message = document.getElementById("message");
+  const email_id = document.getElementById("email");
+  
+  emailjs.send(serviceID, templateID, {
+    from_name: name.value,
+    message: message.value,
+    email_id: email_id.value
+  })
+  .then((response) => {
+    alert("Mensaje Enviado");
+    // Limpiar los valores de input correctamente
+    name.value = "";
+    message.value = "";
+    email_id.value = "";
+  }, (err) => {
+    alert("Mensaje no enviado");
+    console.error("Error al enviar el mensaje: ", err); // Mostrar más detalles del error
+  });
+});
